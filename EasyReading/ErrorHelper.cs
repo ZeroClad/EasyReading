65d394972f506d5bcf1c7f9527d3f08eadc3679e
Zero Chen <zero.clad0@gmail.com>
2020-11-19 14:44:42 +0800

    [Add] taskbar and errorlist

---
+++ EasyReading/ErrorHelper.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.TextManager.Interop;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace EasyReading
+{
+    class ErrorHelper : IServiceProvider
+    {
+        public object GetService(Type serviceType)
+        {
+            return Package.GetGlobalService(serviceType);
+        }
+
+        public ErrorListProvider GetErrorListProvider()
+        {
+            ErrorListProvider provider = new ErrorListProvider(this);//this implementing IServiceProvider
+            provider.ProviderName = "EasyReading";
+            provider.ProviderGuid = FirstCommand.CommandSet;
+            return provider;
+        }
+
+        public void Write(
+            TaskCategory category,
+            TaskErrorCategory errorCategory,
+            string context, //used as an indicator when removing
+            string text,
+            string document,
+            int line,
+            int column)
+        {
+            ErrorTask task = new ErrorTask();
+            task.Text = text;
+            task.ErrorCategory = errorCategory;
+            //The task list does +1 before showing this numbers
+            task.Line = line - 1;
+            task.Column = column - 1;
+            task.Document = document;
+            task.Category = category;
+
+            GetErrorListProvider().Tasks.Add(task);//add it to the errorlistprovider
+        }
+    }
+
+}
---
+++ EasyReading/FirstCommand.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
+
+namespace EasyReading
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class FirstCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 4129;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("05ed4dca-ec94-4fd3-9cec-29e9288fe8b1");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FirstCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private FirstCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static FirstCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in FirstCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new FirstCommand(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            string title = "FirstCommand";
+
+            // Show a message box to prove we were here
+            VsShellUtilities.ShowMessageBox(
+                this.package,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            IVsStatusbar statusBar = this.package.GetService<SVsStatusbar, IVsStatusbar>();
+
+            // Make sure the status bar is not frozen
+            int frozen;
+
+            statusBar.IsFrozen(out frozen);
+
+            if (frozen != 0)
+            {
+                statusBar.FreezeOutput(0);
+            }
+
+            // Set the status bar text and make its display static.
+            statusBar.SetText("We just wrote to the status bar.");
+
+            // Freeze the status bar.
+            statusBar.FreezeOutput(1);
+
+            // Get the status bar text.
+            string text;
+            statusBar.GetText(out text);
+            //System.Windows.Forms.MessageBox.Show(text);
+
+            // Clear the status bar text.
+            statusBar.FreezeOutput(0);
+            statusBar.Clear();
+            ErrorHelper eh = new ErrorHelper();
+            eh.Write(TaskCategory.All, TaskErrorCategory.Error, "context", "text", "document", 123, 321);
+        }
+    }
+}
